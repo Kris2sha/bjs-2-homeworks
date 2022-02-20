439f7edbacb57e102fc41fe7079d32ac24454ed5
kris2sha <kristina.dubovik.006@gmail.com>
2022-02-20 22:42:03 +0300

    changed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,17 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+
+  let arr = {};
+  let D = b*b-4*a*c;
+  if (D>0){
+    let res = [];
+    res.push ((-b + Math.sqrt(D))/(2*a));
+    res.push ((-b - Math.sqrt(D))/(2*a));
+    arr ["Квадратные корни"] = res;
+  } else if (D==0) {
+    arr ["Квадратные корни"] = (-b/(2*a));
+  } else if (D<0){
+    console.log ("Нет решений");
+  }
   return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
