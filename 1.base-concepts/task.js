df2bfa49abeb1d1d5592a2b0039dfa8a7ec2c504
kris2sha <kristina.dubovik.006@gmail.com>
2022-02-21 21:51:46 +0300

    changet task.js3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
 
-  let arr = {};
-  let D = b*b-4*a*c;
-  if (D>0){
-    let res = [];
-    res.push ((-b + Math.sqrt(D))/(2*a));
-    res.push ((-b - Math.sqrt(D))/(2*a));
-    arr = res;
-    console.log (res);
-  } else if (D==0) {
-    let result = [];
-    result.push (-b/(2*a));
-    arr=result;
-    console.log (result);
-  } else if (D<0){
-    console.log ("Нет решений");
-  }
+  let arr = [];
+  let d = b*b-4*a*c;
+  if (d>0){
+    arr.push ((-b + Math.sqrt(d))/(2*a));
+    arr.push ((-b - Math.sqrt(d))/(2*a));
+    console.log (arr);
+  } else if (d==0) {
+    arr.push (-b/(2*a));
+    console.log (arr);
+  } 
   return arr; // array
 }
